17397c931a20b04357edf4470b0cc30e3edc04a9
Max Goedjen <maxwell.j.goedjen@disney.com>
2015-02-18 13:19:20 -0800

    Slack and tests.

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-def testPass():
-	pass
+import slack
+
+def testUsers():
+	users = slack.users()
+	assert 'max' in users
+	assert 'evilmax' not in users
+
+def testChannels():
+	channels = slack.channels()
+	assert 'dma-ios' in channels
+	assert 'dma-ioooooooos' not in channels
+
+def testSharkEmoji():
+	emoji = slack.emoji()
+	assert 'leftshark' in emoji
+	assert 'rightshark' in emoji
+	assert 'middleshark' not in emoji
 	
-def testFailure():
-	assert False
